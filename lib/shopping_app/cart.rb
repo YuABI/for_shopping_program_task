9263e04ef35071666a6d9d63020a19998dae859f
yuabiko <yu.bob.abiko@gmail.com>
2024-02-22 20:15:16 +0900

    first_commit

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -1,6 +1,7 @@
 require_relative "item_manager"
 
 class Cart
+  include Ownable
   include ItemManager
 
   def initialize(owner)
@@ -25,15 +26,22 @@ class Cart
   def check_out
     return if owner.wallet.balance < total_amount
   # ## 要件
+  @items.each do |item|
+    item_owner_wallet = item.owner.wallet
+    self_owner_wallet = self.owner.wallet
   #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
+  self_owner_wallet.withdraw(item.price)
+    item_owner_wallet.deposit(item.price)
   #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+  item.owner = self.owner
+  end
   #   - カートの中身（Cart#items）が空になること。
-
+  @items = []
+  end
   # ## ヒント
   #   - カートのオーナーのウォレット ==> self.owner.wallet
   #   - アイテムのオーナーのウォレット ==> item.owner.wallet
   #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
   #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
-  end
 
 end
--- lib/shopping_app/item.rb
+++ lib/shopping_app/item.rb
@@ -1,4 +1,5 @@
 class Item
+include Ownable
   attr_reader :name, :price
 
   @@instances = []

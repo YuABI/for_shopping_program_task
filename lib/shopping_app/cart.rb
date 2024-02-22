69e7b3d3cb44278ad2313a2e455162f08377c780
yuabiko <yu.bob.abiko@gmail.com>
2024-02-22 20:56:24 +0900

    fourth commit

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -25,22 +25,22 @@ class Cart
 
   def check_out
     return if owner.wallet.balance < total_amount
-  # ## 要件
+   # ## 要件
     @items.each do |item|
      item_owner_wallet = item.owner.wallet
      self_owner_wallet = self.owner.wallet
     end
-  #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
+   #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
    self_owner_wallet.withdraw(item.price)
      item_owner_wallet.deposit(item.price)
-  #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+   #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
    item.owner = self.owner
-  #   - カートの中身（Cart#items）が空になること。
+   #   - カートの中身（Cart#items）が空になること。
    @items = []
-  # ## ヒント
-  #   - カートのオーナーのウォレット ==> self.owner.wallet
-  #   - アイテムのオーナーのウォレット ==> item.owner.wallet
-  #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
-  #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
-   end
+   # ## ヒント
+   #   - カートのオーナーのウォレット ==> self.owner.wallet
+   #   - アイテムのオーナーのウォレット ==> item.owner.wallet
+   #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
+   #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
+  end
 end
